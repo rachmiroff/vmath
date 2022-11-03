39cb55082af4eda152b0f4b362b30b9e0ddaa812
Kutmanbekov Iskender <kutmanbekov_i@auca.kg>
2022-11-03 09:02:57 +0100

    Fix tabs in interpolation methods for Vec2

--- lib/src/test/java/vmath/Vec2Test.java
+++ lib/src/test/java/vmath/Vec2Test.java
@@ -127,15 +127,15 @@ class Vec2Test {
         assertEquals(dot2, -2.0f);
     }
 
-	@Test
-	void linearly_interpolates_two_vectors() {
-		var v1 = new Vec2(1.0f, 2.0f);
+    @Test
+    void linearly_interpolates_two_vectors() {
+        var v1 = new Vec2(1.0f, 2.0f);
         var v2 = new Vec2(2.0f, 3.0f);
-		float alpha = 0.5f;
-		var v3 = v1.lerp(v2, alpha);
-		assertEquals(v3.x(), 1.5f);
-		assertEquals(v3.y(), 2.5f);
-	}
+        float alpha = 0.5f;
+        var v3 = v1.lerp(v2, alpha);
+        assertEquals(v3.x(), 1.5f);
+        assertEquals(v3.y(), 2.5f);
+    }
 
     @Test
     void converts_zero_vector_to_string() {
