d87aaa2df1b261adef8c34f66fb4d3be643f4622
Sardar Sultanaliev <sultanaliev_s@auca.kg>
2022-11-19 18:07:43 +0100

    Create Mat3 constructors skeletons

---
+++ lib/src/main/java/vmath/Mat3.java
@@ -0,0 +1,17 @@
+package vmath;
+
+public class Mat3 {
+    private float[] m;
+
+    public Mat3() {
+    }
+
+    public Mat3(
+            float m00, float m01, float m02,
+            float m10, float m11, float m12,
+            float m20, float m21, float m22) {
+    }
+
+    public Mat3(float arr[]) {
+    }
+}
