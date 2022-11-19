5638a9117e405e4a8d79030ae934f443d2b54649
kalysbek_z <kalysbek_z@auca.kg>
2022-11-19 13:17:10 +0100

    Pass the 'creates_new_matrix_with_args', 'fails_to_creates_new_matrix_with_nan_args' tests for Mat2

--- lib/src/main/java/vmath/Mat2.java
+++ lib/src/main/java/vmath/Mat2.java
@@ -7,6 +7,17 @@ public class Mat2 {
         m = new float[4];
     }
 
+    public Mat2(float m00, float m01, float m10, float m11) {
+        if (Float.isNaN(m00) || Float.isNaN(m01) || Float.isNaN(m10) || Float.isNaN(m11)) {
+            throw new ArithmeticException();
+        }
+        m = new float[4];
+        m[0] = m00;
+        m[1] = m01;
+        m[2] = m10;
+        m[3] = m11;
+    }
+
     public float get(int i, int j) {
         return m[(i * 2) + j];
     }
