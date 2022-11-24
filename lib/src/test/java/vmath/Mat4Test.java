40c722d24150716c00e52238ce9a3e5c34e9c1b0
Tilek Torogeldiev <tilek2402@gmail.com>
2022-11-24 06:10:50 +0100

    Pass 'converts_identity_mat_to_string', 'converts_int_param_mat_to_string' and 'converts_real_param_mat_to_string' tests for Mat4

--- lib/src/test/java/vmath/Mat4Test.java
+++ lib/src/test/java/vmath/Mat4Test.java
@@ -131,4 +131,34 @@ public class Mat4Test {
             assertThrows(ArithmeticException.class, () -> new Mat4(vals));
         }
     }
+
+    @Test
+    void converts_identity_mat_to_string() {
+        var m = new Mat4();
+        assertEquals(m.toString(), "{(1.0, 0.0, 0.0, 0.0), (0.0, 1.0, 0.0, 0.0), (0.0, 0.0, 1.0, 0.0), (0.0, 0.0, 0.0, 1.0)}");
+    }
+
+    @Test
+    void converts_int_param_mat_to_string() {
+        var vals = new float[] {
+            1.0f,  2.0f,  3.0f,  4.0f,
+            5.0f,  6.0f,  7.0f,  8.0f,
+            9.0f,  10.0f, 11.0f, 12.0f,
+            13.0f, 14.0f, 15.0f, 16.0f
+        };
+        var v2 = new Mat4(vals);
+        assertEquals(v2.toString(), "{(1.0, 2.0, 3.0, 4.0), (5.0, 6.0, 7.0, 8.0), (9.0, 10.0, 11.0, 12.0), (13.0, 14.0, 15.0, 16.0)}");
+    }
+
+    @Test
+    void converts_real_param_mat_to_string() {
+        var vals = new float[] {
+            0.5f,     0.25f,     0.125f,     0.0625f,
+            0.03125f, 0.015625f, 0.0078125f, 0.00390625f,
+            0.6f,     0.36f,     0.216f,     0.1296f,
+            0.7776f,  0.046656f, 0.0279936f, 0.01679616f
+        };
+        var v3 = new Mat4(vals);
+        assertEquals(v3.toString(), "{(0.5, 0.25, 0.125, 0.0625), (0.03125, 0.015625, 0.0078125, 0.00390625), (0.6, 0.36, 0.216, 0.1296), (0.7776, 0.046656, 0.0279936, 0.01679616)}");
+    }
 }
