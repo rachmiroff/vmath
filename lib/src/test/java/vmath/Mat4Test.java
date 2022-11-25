dd1eaaf6eed1914c0f7d92bc4d477dfe1a4dbeb4
zimbirnid <zhakshylykov_f@auca.kg>
2022-11-25 18:36:57 +0600

    Add toArray() method in Mat4

--- lib/src/test/java/vmath/Mat4Test.java
+++ lib/src/test/java/vmath/Mat4Test.java
@@ -131,20 +131,4 @@ public class Mat4Test {
             assertThrows(ArithmeticException.class, () -> new Mat4(vals));
         }
     }
-
-    @Test
-     void converts_matrix_to_array_of_floats() {
-         float[] array = {
-            1.0f, 2.0f, 3.0f, 4.0f,
-            5.0f, 6.0f, 7.0f, 8.0f, 
-            9.0f, 10.0f, 11.0f, 12.0f,
-            13.0f, 14.0f, 15.0f, 16.0f
-         };
-         var m = new Mat4(array);
-         var arr = m.toArray();
-         for (int i = 0; i < array.length; i++) {
-            assertEquals(array[i], arr[i]);
-         }
-         assertEquals(1.0f, m.toArray()[0]);
-     }
 }
