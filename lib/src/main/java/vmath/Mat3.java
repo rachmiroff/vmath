dbbeaf567525babbfc01628646eb475a24afb8ef
Khudododkhonov Sherkhon <khudododkhonov_s@auca.kg>
2022-11-29 07:26:31 +0100

    Create a Unit test/s to get an element for the given indices

--- lib/src/main/java/vmath/Mat3.java
+++ lib/src/main/java/vmath/Mat3.java
@@ -4,22 +4,22 @@ public class Mat3 {
     private final float[] m;
 
     public Mat3() {
-        m = new float[] {
-            1.0f, 0.0f, 0.0f,
-            0.0f, 1.0f, 0.0f,
-            0.0f, 0.0f, 1.0f
+        m = new float[]{
+                1.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 0.0f, 1.0f
         };
     }
 
     public Mat3(
-        float m00, float m01, float m02,
-        float m10, float m11, float m12,
-        float m20, float m21, float m22
+            float m00, float m01, float m02,
+            float m10, float m11, float m12,
+            float m20, float m21, float m22
     ) {
-        var vals = new float[] {
-            m00, m01, m02,
-            m10, m11, m12,
-            m20, m21, m22
+        var vals = new float[]{
+                m00, m01, m02,
+                m10, m11, m12,
+                m20, m21, m22
         };
 
         for (float val : vals) {
@@ -47,5 +47,10 @@ public class Mat3 {
 
     public float get(int i, int j) {
         return m[i * 3 + j];
+    }
+
+
+    public float transpose(int i, int j) {
+        return 0;
     }
 }
